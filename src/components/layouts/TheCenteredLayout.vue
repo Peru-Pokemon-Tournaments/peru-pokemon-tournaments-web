9c45771d681c0e1b1d7358eaf4d8981db02af3be
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-26 17:50:29 -0500

    Added layouts

---
+++ src/components/layouts/TheCenteredLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: auto;
+}
+</style>
