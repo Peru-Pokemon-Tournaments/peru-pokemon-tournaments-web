fe383025696670d89c2f779ecef77c1c76bad6c5
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-26 17:51:33 -0500

    Updated App

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,43 @@
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { defineComponent } from "vue-demi";
 
-export default class App extends Vue {}
+export default defineComponent({});
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "@/styles/_reset.scss";
+
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+form,
+fieldset,
+table,
+tr,
+td,
+img {
+  font-family: "Roboto", Arial, Helvetica, sans-serif !important;
+}
+
+input,
+button,
+select,
+textarea,
+optgroup,
+option {
+  font-family: inherit;
+  font-size: inherit;
+  font-style: inherit;
+  font-weight: inherit;
+}
+
+body {
+  font-size: 0.875em;
+  margin: 0;
 }
 </style>
