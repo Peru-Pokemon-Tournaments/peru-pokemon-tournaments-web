caeaffbf32fb6b43762d8b942abdd04a4a3573f4
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-28 19:06:52 -0500

    Added services uris

---
+++ src/config/services-uri.config.ts
@@ -0,0 +1,4 @@
+export const API_DOMAIN: string =
+  process.env["API_DOMAIN"] ?? "http://localhost:8000";
+export const REGISTER_USER_URI: string = "/api/register";
+export const LOGIN_URI: string = "/api/login";
