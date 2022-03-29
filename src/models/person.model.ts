bd8d1f4861ea47212d1bc5f3d69e21f411ac7553
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-28 21:24:32 -0500

    Added new models

---
+++ src/models/person.model.ts
@@ -0,0 +1,23 @@
+export class Person {
+  constructor(
+    private _id: string,
+    private _firstName: string,
+    private _lastName: string
+  ) {}
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get firstName(): string {
+    return this._firstName;
+  }
+
+  public get lastName(): string {
+    return this._lastName;
+  }
+
+  public static fromJson(json: any): Person {
+    return new Person(json["id"], json["first_name"], json["last_name"]);
+  }
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,35 @@
+import { Person } from "./person.model";
+
+export class User {
+  constructor(
+    private _id: string,
+    private _name: string,
+    private _email: string,
+    private _person: Person
+  ) {}
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+
+  public get person(): Person {
+    return this._person;
+  }
+
+  public static fromJson(json: any): User {
+    return new User(
+      json["id"],
+      json["name"],
+      json["email"],
+      Person.fromJson(json["person"])
+    );
+  }
+}
