c32a752b223ee93faca295aac3fffd2f77158815
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-02 16:45:37 -0500

    Added new tournament endpoints

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -2,3 +2,4 @@ export const API_DOMAIN: string =
   process.env["API_DOMAIN"] ?? "http://localhost:8000";
 export const REGISTER_USER_URI: string = "/api/register";
 export const LOGIN_URI: string = "/api/login";
+export const GET_ALL_TOURNAMENTS: string = "/api/tournaments";
