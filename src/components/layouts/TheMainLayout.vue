aab38ecbcec661444140667c35b180fbefec55e9
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-02 16:49:16 -0500

    Created main layout

---
+++ src/components/layouts/TheMainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <slot name="navbar"></slot>
+    <slot></slot>
+    <slot name="footer"></slot>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+main {
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: auto;
+}
+</style>
