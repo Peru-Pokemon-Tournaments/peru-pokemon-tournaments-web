d2878d550cf004d7f75dc4db2605b5bed73d7118
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-02 16:45:54 -0500

    Added tournament service to providers

--- src/config/providers.config.ts
+++ src/config/providers.config.ts
@@ -1,12 +1,20 @@
 import { ApiAuthService, AuthService } from "@/services/auth.service";
+import {
+  ApiTournamentService,
+  TournamentService,
+} from "@/services/tournament.service";
 import axios, { AxiosInstance } from "axios";
 
 const httpClient: AxiosInstance = axios.create();
 
 const authService: AuthService = new ApiAuthService(httpClient);
+const tournamentService: TournamentService = new ApiTournamentService(
+  httpClient
+);
 
 const ServiceProvider = {
   authService,
+  tournamentService,
 };
 
 export default ServiceProvider;
