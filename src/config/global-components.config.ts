7954daa6c811a94b9a47f2eccaa99ec6fa0b1733
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-02 19:16:24 -0500

    Added base loader

--- src/config/global-components.config.ts
+++ src/config/global-components.config.ts
@@ -5,6 +5,7 @@ import BasePicture from "@/components/ui/images/BasePicture.vue";
 import BaseInput from "@/components/ui/inputs/BaseInput.vue";
 import BaseLabel from "@/components/ui/semantics/BaseLabel.vue";
 import BaseTitle from "@/components/ui/semantics/BaseTitle.vue";
+import BaseLoader from "@/components/ui/loaders/BaseLoader.vue";
 
 export const addComponents = (app: App): void => {
   app.component("base-button", BaseButton);
@@ -13,4 +14,5 @@ export const addComponents = (app: App): void => {
   app.component("base-input", BaseInput);
   app.component("base-label", BaseLabel);
   app.component("base-title", BaseTitle);
+  app.component("base-loader", BaseLoader);
 };
