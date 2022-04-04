93cc283823ec9db06f60df9184f1ef974b468cfa
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 20:17:26 -0500

    Added new endpoints

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -4,3 +4,15 @@ export const REGISTER_USER_URI: string = "/api/register";
 export const LOGIN_URI: string = "/api/login";
 export const GET_ALL_TOURNAMENTS: string = "/api/tournaments";
 export const GET_COMPLETE_TOURNAMENT: string = "/api/tournaments/:tournamentId";
+export const CHECK_ENROLL: string =
+  "/api/tournaments/:tournamentId/competitors/:competitorId/is-enrolled";
+export const ENROLL_TO_TOURNAMENT: string =
+  "/api/tournaments/:tounamentId/inscriptions";
+export const GET_COMPETITORS_IN_TOURNAMENT: string =
+  "/api/tournaments/:tounamentId/inscriptions";
+export const GET_TOURNAMENT_INSCRIPTION: string =
+  "/api/inscriptions/:inscriptionId";
+export const DELETE_TOURNAMENT_INSCRIPTION: string =
+  "/api/inscriptions/:inscriptionId";
+export const UPDATE_TOURNAMENT_INSCRIPTION: string =
+  "/api/inscriptions/:inscriptionId";
