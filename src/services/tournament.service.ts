a7b2c844a2930696736c0b1da83751fd86fc47fb
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 20:16:26 -0500

    Updated tournament service

--- src/services/tournament.service.ts
+++ src/services/tournament.service.ts
@@ -2,15 +2,18 @@ import { AxiosError, AxiosInstance } from "axios";
 import {
   API_DOMAIN,
   GET_ALL_TOURNAMENTS,
+  GET_COMPETITORS_IN_TOURNAMENT,
   GET_COMPLETE_TOURNAMENT,
 } from "@/config/services-uri.config";
 import { ResponseError } from "./interfaces/reponse-error";
 import { Tournament } from "@/models/tournament.model";
 import { CompleteTournament } from "@/models/complete-tournament.model";
+import { Competitor } from "@/models/competitor.model";
 
 export interface TournamentService {
   getAllTournaments(): Promise<Tournament[]>;
   getCompleteTournament(tournamentId: string): Promise<CompleteTournament>;
+  getCompetitorsInTournament(tournamentId: string): Promise<Competitor[]>;
 }
 
 export class ApiTournamentService implements TournamentService {
@@ -48,6 +51,26 @@ export class ApiTournamentService implements TournamentService {
       };
 
       return CompleteTournament.fromJson(data);
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(
+        error.response.data.message,
+        error.response.data.errors
+      );
+    }
+  }
+
+  async getCompetitorsInTournament(
+    tournamentId: string
+  ): Promise<Competitor[]> {
+    try {
+      const response = await this._httpClient.get(
+        API_DOMAIN +
+          GET_COMPETITORS_IN_TOURNAMENT.replace(":tournamentId", tournamentId)
+      );
+
+      return response.data?.competitors?.map((competitor: any) =>
+        Competitor.fromJson(competitor)
+      );
     } catch (error: any | Error | AxiosError) {
       throw new ResponseError(
         error.response.data.message,
