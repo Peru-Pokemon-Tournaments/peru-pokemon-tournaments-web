85a5d69f62ea755b237b1cd5312b6b5dbff2f879
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 20:17:57 -0500

    Added status getters and total competitors count

--- src/models/tournament.model.ts
+++ src/models/tournament.model.ts
@@ -10,9 +10,11 @@ export class Tournament {
     private _place: string,
     private _startDate: Date,
     private _endDate: Date,
+    private _totalCompetitors: number,
     private _format: TournamentFormat,
     private _price: TournamentPrice,
     private _type: TournamentType,
+    private _status: string,
     private _imageUrl?: string
   ) {}
 
@@ -48,6 +50,10 @@ export class Tournament {
     return this._endDate;
   }
 
+  public get totalCompetitors(): number {
+    return this._totalCompetitors;
+  }
+
   public get format(): TournamentFormat {
     return this._format;
   }
@@ -60,6 +66,32 @@ export class Tournament {
     return this._type;
   }
 
+  public get status(): string {
+    return this._status;
+  }
+
+  public get formattedStatus(): string {
+    if (this.status == "IN_PROGRESS") return "Pendiente";
+    if (this.status == "NOT_STARTED") return "Sin empezar";
+    return "ENDED";
+  }
+
+  public get canReceiveInscriptions(): boolean {
+    return this.status == "NOT_STARTED";
+  }
+
+  public get canUpdateInscriptions(): boolean {
+    return this.status == "NOT_STARTED";
+  }
+
+  public get isStarted(): boolean {
+    return this.status == "IN_PROGRESS";
+  }
+
+  public get isEnded(): boolean {
+    return this.status == "ENDED";
+  }
+
   public static fromJson(json: any): Tournament {
     return new Tournament(
       json["id"],
@@ -68,9 +100,11 @@ export class Tournament {
       json["place"],
       new Date(json["start_date"]),
       new Date(json["end_date"]),
+      json["total_competitors"],
       TournamentFormat.fromJson(json["tournament_format"]),
       TournamentPrice.fromJson(json["tournament_price"]),
       TournamentType.fromJson(json["tournament_type"]),
+      json["status"],
       "image" in json && json["image"] !== null ? json["image"]["url"] : null
     );
   }
