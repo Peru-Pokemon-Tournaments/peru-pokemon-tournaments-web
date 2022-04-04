17c73d3827cb4ff7ea6b7220276516dd8c465287
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 20:16:46 -0500

    Updated pinia custom properties

--- src/shims-pinia.d.ts
+++ src/shims-pinia.d.ts
@@ -1,10 +1,12 @@
 import "pinia";
 import { AuthService } from "./services/auth.service";
+import { InscriptionService } from "./services/inscription.service";
 import { TournamentService } from "./services/tournament.service";
 
 declare module "pinia" {
   export interface PiniaCustomProperties {
     authService: AuthService;
     tournamentService: TournamentService;
+    inscriptionService: InscriptionService;
   }
 }
