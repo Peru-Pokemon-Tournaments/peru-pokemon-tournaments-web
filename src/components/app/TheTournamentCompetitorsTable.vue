6900e10569bb48c43dcee4b0e96943e39c133dea
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 21:00:39 -0500

    Created tournament competitors table

---
+++ src/components/app/TheTournamentCompetitorsTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <base-title type="h4">
+    Lista de participantes para el torneo: {{ title }}
+  </base-title>
+  <hr />
+  <table>
+    <thead>
+      <th>Nombres y Apellidos</th>
+      <th>Nickname</th>
+    </thead>
+    <tbody>
+      <tr v-for="competitor of competitors" :key="competitor.id">
+        <td>
+          {{ competitor.fullName }}
+        </td>
+        <td class="nickname">
+          {{ competitor.nickName }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script lang="ts">
+import { Competitor } from "@/models/competitor.model";
+import { defineComponent, PropType } from "vue";
+export default defineComponent({
+  props: {
+    title: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    competitors: {
+      type: Array as PropType<Competitor[]>,
+      required: true,
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+table {
+  width: 100%;
+
+  td,
+  th {
+    border: 1px solid black;
+    padding: 0.5rem;
+  }
+
+  th {
+    font-weight: bold;
+    background-color: rgb(208, 214, 214);
+  }
+
+  .nickname {
+    text-align: center;
+  }
+}
+</style>
