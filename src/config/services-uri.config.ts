c8fb3cd84028ce96dd8e58c94f8c7f30f1b507a6
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-04 23:34:42 -0500

    Changed .env variable name

--- src/config/services-uri.config.ts
+++ src/config/services-uri.config.ts
@@ -1,5 +1,5 @@
 export const API_DOMAIN: string =
-  process.env["API_DOMAIN"] ?? "http://localhost:8000";
+  process.env.VUE_APP_API_DOMAIN ?? "http://localhost:8000";
 export const REGISTER_USER_URI: string = "/api/register";
 export const LOGIN_URI: string = "/api/login";
 export const GET_ALL_TOURNAMENTS: string = "/api/tournaments";
