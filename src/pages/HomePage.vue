4a9ab40704ef228b4a8ff6df02ca2bca014d0938
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-16 16:23:20 -0500

    Added the footer

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,18 +4,23 @@
       <the-navbar></the-navbar>
     </template>
     <the-tournaments></the-tournaments>
+    <template v-slot:footer>
+      <the-footer></the-footer>
+    </template>
   </the-main-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 import TheMainLayout from "@/components/layouts/TheMainLayout.vue";
+import TheFooter from "@/components/app/TheFooter.vue";
 import TheNavbar from "@/components/app/TheNavbar.vue";
 import TheTournaments from "@/components/app/TheTournaments.vue";
 
 export default defineComponent({
   components: {
     TheMainLayout,
+    TheFooter,
     TheNavbar,
     TheTournaments,
   },
