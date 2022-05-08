32ae4016a6546777682eadae6929254f597f5978
Jibaru <ignacioruedaboada@hotmail.com>
2022-05-08 15:03:08 -0500

    Added services to reset and update password

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,6 +3,8 @@ import {
   REGISTER_USER_URI,
   LOGIN_URI,
   API_DOMAIN,
+  RESET_PASSWORD,
+  UPDATE_PASSWORD,
 } from "@/config/services-uri.config";
 import { ResponseError } from "./interfaces/reponse-error";
 import { User } from "@/models/user.model";
@@ -25,6 +27,12 @@ export interface AuthService {
     email: string,
     password: string
   ): Promise<{ user: User; token: string; message: string }>;
+  resetPassword(email: string): Promise<string>;
+  updatePassword(
+    email: string,
+    token: string,
+    newPassword: string
+  ): Promise<string>;
 }
 
 export class ApiAuthService implements AuthService {
@@ -82,6 +90,46 @@ export class ApiAuthService implements AuthService {
       throw new ResponseError(error.response.data.message, {
         fields: [error.response.data.message],
       });
+    }
+  }
+
+  async resetPassword(email: string): Promise<string> {
+    try {
+      const response = await this._httpClient.post(
+        API_DOMAIN + RESET_PASSWORD,
+        {
+          email,
+        }
+      );
+      return response?.data?.message;
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(
+        error.response.data.message,
+        error.response.data.errors
+      );
+    }
+  }
+
+  async updatePassword(
+    email: string,
+    token: string,
+    newPassword: string
+  ): Promise<string> {
+    try {
+      const response = await this._httpClient.put(
+        API_DOMAIN + UPDATE_PASSWORD,
+        {
+          email,
+          token,
+          new_password: newPassword,
+        }
+      );
+      return response?.data?.message;
+    } catch (error: any | Error | AxiosError) {
+      throw new ResponseError(
+        error.response.data.message,
+        error.response.data.errors
+      );
     }
   }
 }
