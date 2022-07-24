8477c0561ba276721201195ae73a25b6cda70965
Jibaru <ignacioruedaboada@hotmail.com>
2022-07-23 23:29:08 -0500

    Updated touranment inscription model

--- src/models/tournament-inscription.model.ts
+++ src/models/tournament-inscription.model.ts
@@ -1,13 +1,19 @@
+import { Competitor } from "./competitor.model";
 import { PokemonShowdownTeam } from "./pokemon-showdown-team.model";
 
 export class TournamentInscription {
   constructor(
     private _id: string,
+    private _competitor: Competitor,
     private _pokemonShowdownTeam: PokemonShowdownTeam
   ) {}
 
   public get id(): string {
     return this._id;
+  }
+
+  public get competitor(): Competitor {
+    return this._competitor;
   }
 
   public get pokemonShowdownTeam(): PokemonShowdownTeam {
@@ -17,6 +23,7 @@ export class TournamentInscription {
   public static fromJson(json: any): TournamentInscription {
     return new TournamentInscription(
       json["id"],
+      Competitor.fromJson(json["competitor"]),
       PokemonShowdownTeam.fromJson(json["pokemon_showdown_team"])
     );
   }
